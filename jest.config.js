1b5968e3b55aed611b14f70f7c853d47d2362f82
SeongOh Park <qkrtjddh1212@naver.com>
2023-12-11 13:39:38 +0900

    👷 [config] webpack -> tsc로 재변경

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  preset: 'ts-jest', //trypeScript파일은 ts-jest에서 CommonJS구문으로 변환
+  preset: 'ts-jest', //typeScript파일은 ts-jest에서 CommonJS구문으로 변환
   testEnvironment: 'node', //테스트 환경
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy', // css, scss 파일을 빈 객체로 변환
+  },
 };
