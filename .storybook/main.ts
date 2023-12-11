a835c54cf949e2271a1bfe31497c6e705b2a1e78
SeongOh Park <qkrtjddh1212@naver.com>
2023-12-11 23:20:41 +0900

    feat[docs]: package docs 제거 및 root에 storybook 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,19 +11,17 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
   stories: [
-    '../../components/src/**/*.@(md|mdx)',
-    '../../components/src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-    // '../../utils/src/**/*.@(md|mdx)',
-    '../../utils/src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-    '../src/stories/README.mdx',
+    '../README.mdx',
+    '../packages/components/src/**/*.@(mdx)',
+    '../packages/components/src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../packages/utils/src/**/*.@(mdx)',
+    '../packages/utils/src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-interactions'),
-    getAbsolutePath('storybook-dark-mode'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
