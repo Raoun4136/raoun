776e86a761147264da1fbe59c167bf3fd6e87ebd
SeongOh Park <qkrtjddh1212@naver.com>
2023-12-28 21:17:07 +0900

    chore(components): jest config 변경

--- packages/components/jest.config.js
+++ packages/components/jest.config.js
@@ -1,3 +1,8 @@
 const config = require('../../jest.config.js');
 
-module.exports = { ...config };
+module.exports = {
+  ...config,
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy', // css, scss 파일을 빈 객체로 변환
+  },
+};
