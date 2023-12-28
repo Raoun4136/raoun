e761bcb9a014a76248a8dfda4155a1a96cb5e067
SeongOh Park <qkrtjddh1212@naver.com>
2023-12-28 13:40:23 +0900

    chore(vanilla-extract): color object export test

--- packages/vanilla-extract/src/index.ts
+++ packages/vanilla-extract/src/index.ts
@@ -1,3 +1,4 @@
 export * from './theme.css';
 export * from './utils';
 export * from './sprinkles.css';
+export * from './typography.css';
--- packages/vanilla-extract/src/sprinkles.css.ts
+++ packages/vanilla-extract/src/sprinkles.css.ts
@@ -5,13 +5,50 @@ import { SemanticColor } from './tokens/semantic/colors';
 import { Space } from './tokens/global/spacing';
 import { Colors } from './tokens/global/colors';
 
-const colors = {
+export const colors = {
   ...transformObject<SemanticColor>(vars.semantic.color),
   ...transformObject<Colors>(vars.global.colors),
 };
-const space = transformObject<Space>({ spacing: vars.global.spacing });
+export const space = transformObject<Space>({ spacing: vars.global.spacing });
 
-const colorProperties = defineProperties({
+export const test_colorProperties = {
+  properties: {
+    color: colors,
+    backgroundColor: colors,
+    borderColor: colors,
+    borderRadius: {
+      none: '0px',
+      medium: '6px',
+      large: '8px',
+      extraLarge: '12px',
+      '2xLarge': '16px',
+      '3xLarge': '24px',
+      '4xLarge': '40px',
+      full: '9999px',
+    },
+    paddingTop: space,
+    paddingBottom: space,
+    paddingLeft: space,
+    paddingRight: space,
+    marginTop: space,
+    marginBottom: space,
+    marginLeft: { ...space, auto: 'auto' },
+    marginRight: { ...space, auto: 'auto' },
+    gap: { ...space, none: 'none' },
+    margin: space,
+  },
+  shorthands: {
+    rounded: ['borderRadius'],
+    padding: ['paddingTop', 'paddingBottom', 'paddingLeft', 'paddingRight'],
+    paddingX: ['paddingLeft', 'paddingRight'],
+    paddingY: ['paddingTop', 'paddingBottom'],
+    margin: ['marginTop', 'marginBottom', 'marginLeft', 'marginRight'],
+    marginX: ['marginLeft', 'marginRight'],
+    marginY: ['marginTop', 'marginBottom'],
+  },
+};
+
+export const colorProperties = defineProperties({
   properties: {
     color: colors,
     backgroundColor: colors,
