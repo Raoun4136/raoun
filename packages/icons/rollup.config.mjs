a73f66fd7ce9f679ca32c21b91d51c01c946e715
SeongOh Park <qkrtjddh1212@naver.com>
2023-12-28 23:29:54 +0900

    chore(icons): rollup 재설정

--- packages/icons/rollup.config.mjs
+++ packages/icons/rollup.config.mjs
@@ -5,6 +5,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import image from '@rollup/plugin-image';
 import dts from 'rollup-plugin-dts';
+import babel from '@rollup/plugin-babel';
 
 export default [
   {
@@ -15,11 +16,13 @@ export default [
         dir: 'dist',
         format: 'cjs',
         sourcemap: true,
+        preserveModules: true,
       },
       {
-        dir: 'dist',
+        dir: 'dist/esm',
         format: 'esm',
         sourcemap: true,
+        preserveModules: true,
       },
     ],
     plugins: [
@@ -31,18 +34,22 @@ export default [
       }),
       typescript({ clean: true, sourceMap: true }),
       // uglify(), // js 압축
+      babel({
+        babelHelpers: 'bundled',
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+      }),
     ],
   }, // 타입 정의 파일 번들링
   {
     input: 'lib/index.ts',
     output: [
       { file: 'dist/index.d.ts', format: 'cjs' },
-      { file: 'dist/index.esm.d.ts', format: 'esm' },
+      { file: 'dist/esm/index.esm.d.ts', format: 'esm' },
     ],
     exports: {
       '.': {
-        import: './dist/index.d.ts',
-        require: './dist/index.esm.d.ts',
+        import: './dist/esm/index.esm.d.ts',
+        require: './dist/index.d.ts',
       },
     },
     plugins: [dts()],
